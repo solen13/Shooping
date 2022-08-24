e715fb4a6b6ed7485cec8b614cba115bf4e640d8
ugur-solen <76258170+solen13@users.noreply.github.com>
2022-08-24 09:33:19 +0300

    update

--- app.js
+++ app.js
@@ -1,257 +1,164 @@
-const sepetin=document.querySelector('#sepetin')
-const sepetins=document.querySelector('#sepetins')
-const singing=document.querySelector('.singing')
-const login=document.querySelector('.loging')
-const body=document.querySelector('body')
-const contener=document.querySelector('.contener')
+const sepetin = document.querySelector("#sepetin");
+const sepetins = document.querySelector("#sepetins");
+const singing = document.querySelector(".singing");
+const login = document.querySelector(".loging");
+const body = document.querySelector("body");
+const contener = document.querySelector(".contener");
 
+sepetin.addEventListener("click", (e) => {
+  sepetins.classList.toggle("sepetgo");
+});
 
+singing.addEventListener("click", (e) => {
+  let t = false;
+  login.classList.add("loging-togle");
 
-sepetin.addEventListener('click',e=>{
-    sepetins.classList.toggle('sepetgo')
-})
+  sepetins.classList.remove("sepetgo");
 
+  body.style.overflow = "hidden";
+});
 
-singing.addEventListener('click',e=>{
-    
-    let t=false;
-    login.classList.add('loging-togle')
+const cikis = document.querySelector(".cikis");
+cikis.addEventListener("click", (e) => {
   
-    sepetins.classList.remove('sepetgo')
+  login.classList.remove("loging-togle");
 
-   
-    body.style.overflow="hidden"
- 
-    
- 
-})
+  body.style.overflow = "auto";
 
 
-  
-   
-      
+});
 
-    
-    
+///silder
+var models = [
+  {
+    image:
+      "https://st1.myideasoft.com/idea/fh/13/myassets/products/760/a54da553-8373-4b23-886b-2981375b57a7_min.jpeg?revision=1655203705",
+    titile: "Takılar da indirim",
+  },
+  {
+    image:
+      "https://www.indigotaki.com/askin-kanatlari-isimli-gumus-kolye-gumus-isimli-kolye-indigo-taki-110289-20-K.jpg",
+    titile: "aksesuarlarda 1 alan 1 bedava",
+  },
+  {
+    image:
+      "https://cdn.sorsware.com/oxxo/ContentImages/Product/22k/22KOX-MOPPATGOM2/oversize-gomlek--modal-_olive-darb-yesil_1_detay.jpg",
+    titile: "İlk bahar kadın kombini",
+  },
+  {
+    image: "https://shop.blu.com.tr/Uploads/EditorUploads/kadin_dik.jpg",
+    titile: "İnalımaz indirmler bu Çarşambay kadar",
+  },
+  {
+    image: "https://www.jakamen.com.tr/Uploads/Slider/-63.jpg?t=20220315092200",
+    titile: "Erkek kombinleri ",
+  },
+  {
+    image:
+      "https://vakko.akinoncdn.com/products/2022/03/09/1298782/6cf84369-9632-455d-9a5f-a556ce7528cc_size470x940_quality100.jpg",
+    titile: "%50 varann indirmileri Kaçırma",
+  },
+];
 
+var index = 0;
+var slaytCount = models.length;
+var interval;
 
+var settings = {
+  duration: "3000",
+  random: false,
+};
 
+init(settings);
 
+document.querySelector(".left-btn").addEventListener("click", function () {
+  index--;
+  showSlide(index);
+});
 
+document.querySelector(".right-btn").addEventListener("click", function () {
+  index++;
+  showSlide(index);
+});
 
+document.querySelectorAll(".arrow").forEach(function (item) {
+  item.addEventListener("mouseenter", function () {
+    clearInterval(interval);
+  });
+});
 
+document.querySelectorAll(".arrow").forEach(function (item) {
+  item.addEventListener("mouseleave", function () {
+    init(settings);
+  });
+});
 
+function init(settings) {
+  var prev;
 
+  interval = setInterval(function () {
+    if (settings.random) {
+      do {
+        index = 0;
+      } while (index == prev);
+      prev = index;
+    } else {
+      if (slaytCount == index + 1) {
+        index = -1;
+      }
+      showSlide(index);
 
+      index++;
+    }
+    showSlide(index);
+  }, settings.duration);
+}
 
+function showSlide(i) {
+  index = i;
 
+  if (i < 0) {
+    index = slaytCount - 1;
+  }
+  if (i >= slaytCount) {
+    index = 0;
+  }
+  document
+    .querySelector(".silder-image")
+    .setAttribute("src", models[index].image);
+  document.querySelector(".silder-titile").textContent = models[index].titile;
+}
 
+let changee = document.querySelector(".radio-control");
 
+for (i = 0; i < models.length; i++) {
+  var divv = document.createElement("div");
+  changee.appendChild(divv);
+  divv.id = i;
+}
 
+let change = document.querySelectorAll(".radio-control");
 
+for (let icon of change) {
+  icon.addEventListener("click", (e) => {
+    if (e.target.id === "0") {
+      index = 0;
+    } else if (e.target.id === "1") {
+      index = 1;
+    } else if (e.target.id === "2") {
+      index = 2;
+    } else if (e.target.id === "3") {
+      index = 3;
+    } else if (e.target.id === "4") {
+      index = 4;
+    } else if (e.target.id === "5") {
+      index = 5;
+    } else if (e.target.id === "6") {
+      index = 6;
+    }
+  });
+}
 
-
-
-
-
-
-
-
-
-const cikis=document.querySelector('.cikis')
-cikis.addEventListener('click',e=>{
-    
-    
-    login.classList.remove('loging-togle')
-  
-    
-
-   
-    body.style.overflow="auto"
- 
-
- 
-})
-
-
-
-
-
-
-
-
-
-
-            ///silder
-            var models = [
-                {
-                   
-                    image : 'https://st1.myideasoft.com/idea/fh/13/myassets/products/760/a54da553-8373-4b23-886b-2981375b57a7_min.jpeg?revision=1655203705',
-                    titile:'Takılar da indirim'
-                  
-                },
-                {
-                    image : 'https://www.indigotaki.com/askin-kanatlari-isimli-gumus-kolye-gumus-isimli-kolye-indigo-taki-110289-20-K.jpg',
-                    titile:'aksesuarlarda 1 alan 1 bedava'
-                  
-                }
-                ,
-                {
-                    image : 'https://cdn.sorsware.com/oxxo/ContentImages/Product/22k/22KOX-MOPPATGOM2/oversize-gomlek--modal-_olive-darb-yesil_1_detay.jpg',
-                    titile:'İlk bahar kadın kombini'
-                  
-                },
-                {
-                    image : 'https://shop.blu.com.tr/Uploads/EditorUploads/kadin_dik.jpg',
-                    titile:'İnalımaz indirmler bu Çarşambay kadar'
-                  
-                }
-                ,
-                {
-                    image : 'https://www.jakamen.com.tr/Uploads/Slider/-63.jpg?t=20220315092200',
-                    titile:'Erkek kombinleri '
-                  
-                },
-                {
-                    image : 'https://vakko.akinoncdn.com/products/2022/03/09/1298782/6cf84369-9632-455d-9a5f-a556ce7528cc_size470x940_quality100.jpg',
-                    titile:'%50 varann indirmileri Kaçırma'
-                  
-                }
-
-            ];
-            
-            var index = 0;
-            var slaytCount = models.length;
-            var interval;
-            
-
-    
-
-
-            var settings={
-                duration : '3000',
-                random : false
-            };
-            
-            init(settings);
-            
-            document.querySelector('.left-btn').addEventListener('click',function(){
-                index--;
-                showSlide(index);
-        
-               
-            });
-            
-            document.querySelector('.right-btn').addEventListener('click',function(){
-                index++;
-                showSlide(index);
-                
-               
-            });
-            
-            document.querySelectorAll('.arrow').forEach(function(item){
-                item.addEventListener('mouseenter',function(){
-                    clearInterval(interval);
-                })
-            });
-
-            
-            document.querySelectorAll('.arrow').forEach(function(item){
-                item.addEventListener('mouseleave',function(){
-                    init(settings);
-                })
-            })
-            
-            
-            function init(settings){
-            
-                var prev;
-
-                interval=setInterval(function(){
-                    
-                 if(settings.random){
-                     
-                     do{
-                     index = 0;
-                     }while(index == prev)
-                     prev = index;
-                          }else{
-                  
-                        if(slaytCount == index+1){
-                            index = -1;
-                        }
-                        showSlide(index);
-                       
-                        index++;
-
-                     
-                    }
-                    showSlide(index);
-            
-                },settings.duration)
-  
-            }
-            
-            
-            
-            function showSlide(i){
-            
-                index = i;
-            
-                if (i<0) {
-                    index = slaytCount - 1;
-                }
-                if(i >= slaytCount){
-                    index =0;
-                }
-                document.querySelector('.silder-image').setAttribute('src',models[index].image);
-                document.querySelector('.silder-titile').textContent=models[index].titile;  
-            }
-
-        
-
-            let changee=document.querySelector('.radio-control')
-            
-            for(i=0;i<models.length;i++){           
-                var divv=document.createElement('div')
-                changee.appendChild(divv)             
-                divv.id=i   
-             
-            }
-    
-             let change=document.querySelectorAll('.radio-control')
-
-         for(let icon of change){
-        icon.addEventListener('click',e=>{
-            if(e.target.id==='0'){
-                index=0
-            }
-            else if(e.target.id==='1'){
-            index=1         
-            }
-            else if(e.target.id==='2'){
-            index=2             
-            }
-            else if(e.target.id==='3'){
-                index=3                  
-                }
-            else if(e.target.id==='4'){
-                 index=4                  
-                 }
-            else if(e.target.id==='5'){
-                 index=5           
-                 }
-             else if(e.target.id==='6'){
-                  index=6          
-                  }
-                          
-        })
-        
-        
-        }
-
-    
-       
 /*
         -silder da resime göre raido btnler aktiflik olayı yapılacak
         -banner a card tasarımı yapılacak (yapıldı)
@@ -261,21 +168,4 @@ cikis.addEventListener('click',e=>{
 
 
 
-*/ 
-
-
-
-        
-      
-
-
-
-        
-        
-
-          
-
-            
-            
-            
-            
+*/
--- script.js
+++ script.js
@@ -1,142 +1,96 @@
+const cards1 = [
+  {
+    image:
+      "https://www.dipmoda.com/UserFiles/Product/list/Yesil-yeni-sezon-cepli-salvar-pantolon-1-.jpg",
+    urunad: "Salaş elbise",
+    fiyat: "150",
+    catagry: "kadın",
+  },
+  {
+    image:
+      "https://productimages.hepsiburada.net/l/44/600-800/10798890549298.jpg",
+    urunad: "Kısa etek",
+    fiyat: "650",
+    catagry: "kadın",
+  },
+  {
+    image:
+      "https://i0.wp.com/seyhanevintage.com/wp-content/uploads/vintage-giyim-kadin-67-1-scaled.jpg?resize=510%2C612&ssl=1",
+    urunad: "Kadın kazak",
+    fiyat: "100",
+    catagry: "kadın",
+  },
+  {
+    image:
+      "https://vakko.akinoncdn.com/products/2020/02/18/506678/614d152e-70fe-46b8-ba14-d004482ca609_size470x940_quality100.jpg",
+    urunad: "Kadın kazak",
+    fiyat: "10",
+    catagry: "erkek",
+  },
+  {
+    image:
+      "https://i.pinimg.com/736x/23/f4/de/23f4de3b38b1f0dc63426f410c22e0eb.jpg",
+    urunad: "Kadın kazak",
+    fiyat: "20",
+    catagry: "erkek",
+  },
+  {
+    image:
+      "https://i.pinimg.com/736x/23/f4/de/23f4de3b38b1f0dc63426f410c22e0eb.jpg",
+    urunad: "Kadın kazak",
+    fiyat: "20",
+    catagry: "erkek",
+  },
+  {
+    image:
+      "https://i.pinimg.com/736x/23/f4/de/23f4de3b38b1f0dc63426f410c22e0eb.jpg",
+    urunad: "Kadın kazak",
+    fiyat: "20",
+    catagry: "erkek",
+  },
+  {
+    image:
+      "https://i.pinimg.com/736x/23/f4/de/23f4de3b38b1f0dc63426f410c22e0eb.jpg",
+    urunad: "Kadın kazak",
+    fiyat: "20",
+    catagry: "erkek",
+  },
+  {
+    image:
+      "https://cdn.beymen.com/mnresize/404/562/productimages/45ztbxtv.mjh_IMG_01_2110083586402.jpg",
+    urunad: "Kadın kazak",
+    fiyat: "175",
+    catagry: "kadın",
+  },
+  {
+    image:
+      "https://cdn.sorsware.com/jimmykey/ContentImages/Product/2022kis/22K08GINA/bol-kesim-arkasi-uzun-100-pamuk-basic-gomlek_22k08gina_acik-sari-206-sari_1_614x805.jpg",
+    urunad: "Kadın kazak",
+    fiyat: "75",
+    catagry: "kadın",
+  },
+];
 
+const newCata = [
+  {
+    catagoy: "kadın",
+  },
 
-const cards1=[
-    {
-       
-        image:'https://www.dipmoda.com/UserFiles/Product/list/Yesil-yeni-sezon-cepli-salvar-pantolon-1-.jpg',
-        urunad:'Salaş elbise',
-        fiyat:'150',
-        catagry:'kadın'
+  {
+    catagoy: "erkek",
+  },
+];
 
-    },
-    {
-       
-      image:'https://productimages.hepsiburada.net/l/44/600-800/10798890549298.jpg',
-      urunad:'Kısa etek',
-      fiyat:'650',
-      catagry:'kadın'
+newcard(newCata[0].catagoy);
 
-    }
-    ,
-  
-    {
-       
-       image:'https://i0.wp.com/seyhanevintage.com/wp-content/uploads/vintage-giyim-kadin-67-1-scaled.jpg?resize=510%2C612&ssl=1',
-       urunad:'Kadın kazak',
-       fiyat:'100',
-       catagry:'kadın'
+newcardd();
 
-    }
-    ,
-    {
-       
-       image:'https://vakko.akinoncdn.com/products/2020/02/18/506678/614d152e-70fe-46b8-ba14-d004482ca609_size470x940_quality100.jpg',
-       urunad:'Kadın kazak',
-       fiyat:'10',
-       catagry:'erkek'
+function newcardd() {
+  const banner = document.querySelector("#card-contener2");
 
-    }
-    ,
-
-    {
-       
-       image:'https://i.pinimg.com/736x/23/f4/de/23f4de3b38b1f0dc63426f410c22e0eb.jpg',
-       urunad:'Kadın kazak',
-       fiyat:'20',
-       catagry:'erkek'
-
-    }
-    ,
-
-    {
-       
-       image:'https://i.pinimg.com/736x/23/f4/de/23f4de3b38b1f0dc63426f410c22e0eb.jpg',
-       urunad:'Kadın kazak',
-       fiyat:'20',
-       catagry:'erkek'
-
-    }
-    ,
-
-    {
-       
-       image:'https://i.pinimg.com/736x/23/f4/de/23f4de3b38b1f0dc63426f410c22e0eb.jpg',
-       urunad:'Kadın kazak',
-       fiyat:'20',
-       catagry:'erkek'
-
-    }
-    ,
-
-    {
-       
-       image:'https://i.pinimg.com/736x/23/f4/de/23f4de3b38b1f0dc63426f410c22e0eb.jpg',
-       urunad:'Kadın kazak',
-       fiyat:'20',
-       catagry:'erkek'
-
-    }
-    ,
-    {
-       
-       image:'https://cdn.beymen.com/mnresize/404/562/productimages/45ztbxtv.mjh_IMG_01_2110083586402.jpg',
-       urunad:'Kadın kazak',
-       fiyat:'175',
-       catagry:'kadın'
-
-    }
-    ,
-    {
-       
-       image:'https://cdn.sorsware.com/jimmykey/ContentImages/Product/2022kis/22K08GINA/bol-kesim-arkasi-uzun-100-pamuk-basic-gomlek_22k08gina_acik-sari-206-sari_1_614x805.jpg',
-       urunad:'Kadın kazak',
-       fiyat:'75',
-       catagry:'kadın'
-
-    }
-
-]
-
-const newCata=[
- 
-    {
-        catagoy:'kadın'
-    },
-
-   {
-        catagoy:'erkek'
-    },
-
-]
-
-    
-
-   newcard(newCata[0].catagoy)
- 
-
-
-
-
-
-
-  
-
-
-
-newcardd()
-
-
-
-   function newcardd(){
-
-
-    const banner=document.querySelector('#card-contener2')
-
-    for(i=0 ;i<cards1.length;i++){
-
-        if(cards1[i].catagry==='erkek'){
-           
-            const html = `
+  for (i = 0; i < cards1.length; i++) {
+    if (cards1[i].catagry === "erkek") {
+      const html = `
             <div class="card-content">
            
                 <div class="card-img-contner">
@@ -154,30 +108,18 @@ newcardd()
                 <button class="btn-ekle">Sepete ekle</button>
            
            
-           </div>`
-           banner.innerHTML +=html
-        }
-       
-        
-      
+           </div>`;
+      banner.innerHTML += html;
     }
-
+  }
 }
 
+function newcard(k) {
+  const banner = document.querySelector(".card-contener");
 
- 
-
-
-    function newcard(k){
-
-
-        const banner=document.querySelector('.card-contener')
-
-        for(i=0 ;i<cards1.length;i++){
-
-            if(cards1[i].catagry===k){
-               
-                const html = `
+  for (i = 0; i < cards1.length; i++) {
+    if (cards1[i].catagry === k) {
+      const html = `
                 <div class="card-content">
                
                     <div class="card-img-contner">
@@ -195,35 +137,25 @@ newcardd()
                     <button class="btn-ekle">Sepete ekle</button>
                
                
-               </div>`
-               banner.innerHTML +=html
-            }
-           
-            
-          
-        }
-
+               </div>`;
+      banner.innerHTML += html;
     }
-    class Shopping{
-        constructor(cardimg,urunBaslik,fiyat){
-            this.cardimg=cardimg
-            this.urunBaslik=urunBaslik
-            this.fiyat=fiyat
+  }
+}
+class Shopping {
+  constructor(cardimg, urunBaslik, fiyat) {
+    this.cardimg = cardimg;
+    this.urunBaslik = urunBaslik;
+    this.fiyat = fiyat;
+  }
+}
+const sepetNew = document.querySelector(".list-item");
 
-           
-        }
+class UI {
+  addCard(shopping) {
+    console.log(sepetNew.length);
 
-
-    }
-    const sepetNew=document.querySelector('.list-item');
-   
-    class UI{
-        addCard(shopping){
-                
-                console.log(sepetNew.length)
-                
-            sepetNew.innerHTML +=
-            `
+    sepetNew.innerHTML += `
             <div class="sepet-content-1">
         <div class="sepet-content-img">
         <img class="sepet-img" src="${shopping.cardimg}" alt="">
@@ -232,133 +164,173 @@ newcardd()
             <h2 class="sepet-urunad">${shopping.urunBaslik}</h2>
             <h3 class="urunfiyats">${shopping.fiyat}</h2>
         </div>
-        <div class="toplamm">  </div>
+        <div class="toplamm">${shopping.fiyat}   </div>
         <div class="urun-adet">
-        <div class="eksi">-</div>
-        <div class="number">1</div>
-        <div class="artı">+</div>
+            <div class="eksi">-</div>
+            <div class="number">1</div>
+            <div class="artı">+</div>
     </div>
     </div>
             
        
-            `
+            `;
+  }
+}
 
+const btnEkle = document.getElementsByClassName("btn-ekle");
+const cardContent = document.getElementsByClassName("card-content");
 
-           
+for (let i = 0; i < cardContent.length; i++) {
+  btnEkle[i].addEventListener("click", function () {
+    let cardimg = cardContent[i].getElementsByClassName("card-img")[0].src;
+    let urunBaslik =
+      cardContent[i].getElementsByClassName("urun-ad")[0].textContent;
+    let fiyat =
+      cardContent[i].getElementsByClassName("card-urun-fiyat")[0].textContent;
+
+    let shopping = new Shopping(cardimg, urunBaslik, fiyat);
+    let ui = new UI();
+    ui.addCard(shopping);
+
+    cartCount();
+    btnEkle[i].disabled = true;
+    btnEkle[i].style.background = "black";
+  });
+}
+
+function urunSayısı() {}
+
+function cartCount() {
+  let tmp1 = 0;
+  let tmp2 = 0;
+  let tmp3 = 0;
+  let tmp4 = 0;
+  let tmp5 = 0;
+
+  let cartListItem = document.querySelectorAll(".sepet-content-1");
+  let itemCount = document.querySelector(".sepet-sayi");
+  let numara = document.querySelectorAll(".number");
+  let urunadet = document.querySelectorAll(".urun-adet");
+  console.log(cartListItem.length);
+  itemCount.textContent = cartListItem.length;
+
+  const arti = document.querySelectorAll(".artı");
+  const eksi = document.querySelectorAll(".eksi");
+  const sepetToplam = document.querySelector(".sepet-toplam");
+  const urunfiyats = document.querySelectorAll(".urunfiyats");
+
+  const toplam = document.querySelectorAll(".toplamm");
+
+  let urunDegistir1=1,urunDegistir2 = 1; 
+  let urunDegistir3 = 1;
+  let urunDegistir4 = 1;
+  let urunDegistir5 = 1;
+
+  for (let i = 0; i < cartListItem.length; i++) {
+    numara[i].classList.add("card" + i);
+
+    if (itemCount.textContent === "5") {
+      btnEkle[i].disabled = true;
+    }
+
+    arti[i].addEventListener("click", (e) => {
+      if (numara[i].classList.contains("card0")) {
+        urunDegistir1++;
+        numara[i].textContent = urunDegistir1;
+        tmp1 = urunDegistir1 * urunfiyats[i].textContent;
+      } else if (numara[i].classList.contains("card1")) {
+        urunDegistir2++;
+        numara[i].textContent = urunDegistir2;
+
+        tmp2 = urunDegistir2 * urunfiyats[i].textContent;
+      } else if (numara[i].classList.contains("card2")) {
+        urunDegistir3++;
+        numara[i].textContent = urunDegistir3;
+
+        tmp3 = urunDegistir3 * urunfiyats[i].textContent;
+      } else if (numara[i].classList.contains("card3")) {
+        urunDegistir4++;
+        numara[i].textContent = urunDegistir4;
+        tmp4 = urunDegistir4 * urunfiyats[i].textContent;
+      } else if (numara[i].classList.contains("card4")) {
+        urunDegistir5++;
+        numara[i].textContent = urunDegistir5;
+        tmp5 = urunDegistir5 * urunfiyats[i].textContent;
+      }
+
+      sepetToplam.textContent = tmp1 + tmp2 + tmp3 + tmp4 + tmp5;
+    });
+
+    eksi[i].addEventListener("click", (e) => {
+      if (numara[i].classList.contains("card0")) {
+        urunDegistir1--;
+        if (urunDegistir1 <= 1) {
+          urunDegistir1 = 1;
         }
-    }
+        numara[i].textContent = urunDegistir1;
 
-    const btnEkle= document.getElementsByClassName('btn-ekle')
-    const cardContent=document.getElementsByClassName('card-content')
+        tmp1 = toplam[i].textContent =
+          urunDegistir1 * urunfiyats[i].textContent;
 
-    for(let i=0;i<cardContent.length;i++){
-        
-       btnEkle[i].addEventListener('click',function(){
+        console.log(tmp1.textContent);
+      } else if (numara[i].classList.contains("card1")) {
+        urunDegistir2--;
+        if (urunDegistir2 <= 1) {
+          urunDegistir2 = 1;
+        }
+        numara[i].textContent = urunDegistir2;
 
+        tmp2 = toplam[i].textContent =
+          urunDegistir2 * urunfiyats[i].textContent;
+      } else if (numara[i].classList.contains("card2")) {
+        urunDegistir3--;
+        if (urunDegistir3 <= 1) {
+          urunDegistir3 = 1;
+        }
+        numara[i].textContent = urunDegistir3;
 
-         let cardimg =  cardContent[i]. getElementsByClassName("card-img")[0].src
-         let urunBaslik= cardContent[i]. getElementsByClassName('urun-ad')[0].textContent
-         let fiyat=    cardContent[i]. getElementsByClassName('card-urun-fiyat')[0].textContent
+        tmp3 = toplam[i].textContent =
+          urunDegistir3 * urunfiyats[i].textContent;
+      } else if (numara[i].classList.contains("card3")) {
+        urunDegistir4--;
+        numara[i].textContent = urunDegistir4;
+        tmp4 = toplam[i].textContent =
+          urunDegistir4 * urunfiyats[i].textContent;
 
-
-
-      let shopping=new Shopping(cardimg,urunBaslik,fiyat)
-        let ui=new UI()
-      ui.addCard(shopping)
-
-
-       cartCount(fiyat)
-       
-       })
-       
-
-    }
-    
-
-   
-
-
-
-    
-    
-       
-    function urunSayısı(){
-               
-
-    
-    
-    }
-
-
-
-   function cartCount( fiyat){
-  
-             let cartListItem =document.querySelectorAll('.sepet-content-1')
-            let itemCount = document.querySelector('.sepet-sayi');
-
-                  console.log(cartListItem.length)
-                  itemCount.textContent=cartListItem.length
-
-        
-                  let urunDegistir=1
-
-                  const num=document.querySelectorAll('.number')
-                  const arti=document.querySelectorAll('.artı')
-                  const eksi=document.querySelectorAll('.eksi')
-                  const sepetToplam=document.querySelectorAll('.sepet-toplam')
-                  const urunfiyats=document.querySelectorAll('.urunfiyats')
-      
-                  const toplam=document.querySelectorAll('.toplamm')
-                        
-                  for(let i=0;i<cartListItem.length;i++){
-                         
-                   
-                         arti[i].addEventListener('click',e=>
-                    {
-   
-                         if(urunDegistir=>1){
-
-                               urunDegistir++
-             
-
-
-                                num[i].textContent=urunDegistir
-                            
-                                
-                            
-                                toplam[i].textContent=num[i].textContent*fiyat
-                                
-        
-                            }
-
-     
-  
-
-    })
-
-    eksi[i].addEventListener('click',e=>{
-         if(urunDegistir<=1){
-             urunDegistir=1
-         }
-         else if(urunDegistir>1){
-             urunDegistir--
-             toplam[i].textContent=toplam[i].textContent-fiyat
-          
-         }
-         num[i].textContent=urunDegistir
-         
-    })
-}}
-
-
-    
-    
-
-      
+        if (urunDegistir4 <= 1) {
+          urunDegistir4 = 1;
+        }
+      } else if (numara[i].classList.contains("card4")) {
+        urunDegistir5--;
+        if (urunDegistir5 <= 1) {
+          urunDegistir5 = 1;
+        }
+        numara[i].textContent = urunDegistir5;
+        tmp5 = toplam[i].textContent =
+          urunDegistir5 * urunfiyats[i].textContent;
+      }
+    });
+  }
+}
 
 
 
 
-        
+///login 
+const signInBtn = document.getElementById("signIn");
+const signUpBtn = document.getElementById("signUp");
+const fistForm = document.getElementById("form1");
+const secondForm = document.getElementById("form2");
+const container = document.querySelector(".container");
 
+signInBtn.addEventListener("click", () => {
+	container.classList.remove("right-panel-active");
+});
+
+signUpBtn.addEventListener("click", () => {
+	container.classList.add("right-panel-active");
+});
+
+fistForm.addEventListener("submit", (e) => e.preventDefault());
+secondForm.addEventListener("submit", (e) => e.preventDefault());
 
